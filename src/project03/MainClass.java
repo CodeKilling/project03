5ff0c5491ebf9e328ba047d68fb4982334d626da
parksunyoung <iss002@naver.com>
2021-08-27 21:42:54 +0900

    psy:: complete

--- src/project03/MainClass.java
+++ src/project03/MainClass.java
@@ -2,12 +2,16 @@ package project03;
 
 import java.util.Scanner;
 
+import psy.PsyMenu;
+
 public class MainClass {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
 		int input = 0;
+		
+		PsyMenu psy = new PsyMenu();
 		
 		while(true) {
 			System.out.println("1.박상진");
@@ -23,7 +27,7 @@ public class MainClass {
 			case 2: break;
 			case 3: break;
 			case 4: break;
-			case 5: break;
+			case 5: psy.display(); break;
 			
 			case 6: sc.close(); System.exit(0);
 			
